2b4e304d96842995df4dc7fd00f1ff7084fd60bd
Arun9348 <arun9948089348@gmail.com>
2025-03-22 07:13:27 +0530

    all services images are added, scroll bar arrow for complete page added,

--- JS/main.js
+++ JS/main.js
@@ -204,6 +204,36 @@
 
 	// Initialize gallery and load the first video by default
 	function initGallery() {
+
+		let offset = 50;
+		let circleContainer = document.querySelector(".circle-container");
+		let circlePath = document.querySelector('.circle-container path');
+		let pathLength = circlePath.getTotalLength();
+		circlePath.style.transition = circlePath.style.WebkitTransition = 'none';
+		circlePath.style.strokeDasharray = pathLength;
+		circlePath.style.strokeDashoffset = pathLength;
+		circlePath.getBoundingClientRect();
+		circlePath.style.transition = circlePath.style.WebkitTransition = 'stroke-dashoffset 10ms linear';
+		let updateLoader = () => {
+			let scrollTop = window.scrollY;
+			let docHeight = document.body.offsetHeight;
+			let winHeight = window.innerHeight;
+			let height = docHeight - winHeight;
+			let progress = pathLength - (scrollTop * pathLength / height);
+			circlePath.style.strokeDashoffset = progress;
+			if (scrollTop > offset) {
+				circleContainer.classList.add("active");
+			} else {
+				circleContainer.classList.remove("active");
+			}
+		}
+		circleContainer.onclick = function () {
+			window.scrollTo({ top: 0, behavior: 'smooth' });
+		}
+		window.onscroll = () => {
+			updateLoader();
+		}
+		updateLoader();
 		const previewsContainer = document.getElementById('videoPreviews');
 		const mainVideoContainer = document.getElementById('mainVideo');
 		const videoInfoContainer = document.getElementById('videoInfo');
